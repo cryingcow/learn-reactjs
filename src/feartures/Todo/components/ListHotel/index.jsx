4b6836157cca5295700615adfa01dddf7462273d
cryingcow <duykhang4568@gmail.com>
2022-04-27 11:47:07 +0700

    update

---
+++ src/feartures/Todo/components/ListHotel/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Hotel from '../Hotel';
+
+ListHotel.propTypes = {
+    
+};
+
+function ListHotel(props) {
+    
+    return (
+        <div>
+            <Hotel/>
+        </div>
+    );
+}
+
+export default ListHotel;
